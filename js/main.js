3538b8aa0b3bfbb7b78021bc135893801b2e780e
Sudharshan <sudharshanj123@gmail.com>
2024-08-30 19:17:22 +0530

    added new page with similar to harshshop.html with hover menu feature

--- js/main.js
+++ js/main.js
@@ -11,21 +11,21 @@
     };
     spinner();
     
-    $(document).ready(function() {
-        // Initially hide the hover menu
-        $('.hover-menu').hide();
+    // $(document).ready(function() {
+    //     // Initially hide the hover menu
+    //     $('.hover-menu').hide();
     
-        $(window).scroll(function() {
-            // Check if the window has been scrolled down 400px or more
-            if ($(this).scrollTop() > 400) {
-                // Show the hover menu
-                $('.hover-menu').fadeIn();
-            } else {
-                // Hide the hover menu
-                $('.hover-menu').fadeOut();
-            }
-        });
-    });    
+    //     $(window).scroll(function() {
+    //         // Check if the window has been scrolled down 400px or more
+    //         if ($(this).scrollTop() > 400) {
+    //             // Show the hover menu
+    //             $('.hover-menu').fadeIn();
+    //         } else {
+    //             // Hide the hover menu
+    //             $('.hover-menu').fadeOut();
+    //         }
+    //     });
+    // });    
 
 
 })(jQuery);
